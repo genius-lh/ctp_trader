c9c290fc30fa3e5a16ff790ef5f44558f15aff06
Lu Hui <25143100@qq.com>
2022-11-07 22:02:48 +0800

    光大udp行情

--- src/api/trader_mduser_api_gd.cpp
+++ src/api/trader_mduser_api_gd.cpp
@@ -9,18 +9,48 @@
 #include <fcntl.h>
 #include <arpa/inet.h>
 
-#include "xele_md_data_if.h"
-
-#define GDXELE_LOG(...) printf(__VA_ARGS__)
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "queue.h"
+#pragma pack(1)
+typedef unsigned int uInt32;
+
+struct PriceLevel
+{
+    double     bid_price;             // 买价
+    uint32_t   bid_volume;            // 买量
+    uint32_t   bid_impvolume;         // 买推导量 上期为常值0
+    double     ask_price;             // 卖价
+    uint32_t   ask_volume;            // 卖量
+    uint32_t   ask_impvolume;         // 卖推导量 上期为常值0
+};
+
+struct ShfeDataField
+{
+    char       instrument_id[20];     // 合约代码
+    uint32_t   update_time;           // 更新时间
+    double     upper_limit;           // 涨停价
+    double     lower_limit;           // 跌停价
+    double     last_price;            // 最新价
+    double     turnover;              // 成交金额
+    uint64_t   volume;                // 成交量
+    uint64_t   open_interest;         // 持仓量
+    PriceLevel depth[5];              // 5档行情     
+    short      instrument_no;         // 合约编码
+    uint8_t    reserve;               // 保留字段
+    uint8_t    exflag;                // 市场标志 1：中金所 2：上期所 3：大商所 4：郑商所 5：上能所
+    uint32_t   seq;                   // 报文序号
+};
+
+#pragma pack()
+
+
+#define GFXELE_LOG(...) printf(__VA_ARGS__)
 
 #include "trader_data.h"
 #include "trader_mduser_api.h"
+#include "dict.h"
 
 extern trader_mduser_api_method* trader_mduser_api_gd_method_get();
 
@@ -31,24 +61,17 @@ static void trader_mduser_api_gd_logout(trader_mduser_api* self);
 static void trader_mduser_api_gd_subscribe(trader_mduser_api* self, char* instrument);
 
 static void* trader_mduser_api_gd_thread(void* arg);
-static void* trader_mduser_api_gd_tcp_thread(void* arg);
 
 static int trader_mduser_api_gd_prase_url(const char* url, char* local_host, char* remote_host, int* port);
-static void gd_mduser_on_rtn_depth_market_data(void* arg, CXeleCffexLevelOneMarketData *pMarketData);
+static void gd_x10_mduser_on_rtn_depth_market_data(void* arg, struct ShfeDataField *pMarketData);
 
 #ifdef __cplusplus
 }
 #endif
 
-typedef struct xele_md_static_inf_def xele_md_static_inf;
-struct xele_md_static_inf_def{
-  TAILQ_ENTRY(xele_md_static_inf_def) next;
-  struct CXeleCffexMarketDataStaticField data;
-};
-
 ///最大的接收缓冲区最
-#define	RCV_BUF_SIZE						65535
-#define	MSG_MAX_LEN						65535
+#define	RCV_BUF_SIZE (8 * 1024 * 1024)
+#define MSG_BUF_SIZE (64 * 1024)
 
 typedef struct trader_mduser_api_gd_def trader_mduser_api_gd;
 struct trader_mduser_api_gd_def{
@@ -57,27 +80,84 @@ struct trader_mduser_api_gd_def{
 	char local_ip[16];				///< 本地IP
 	pthread_t thread_id;
   int loop_flag;
-  pthread_mutex_t thread_mutex;
-	pthread_t thread_tcp_id;
-
-  TAILQ_HEAD(xele_md_static_inf_list, xele_md_static_inf_def) md_static_list;
-  int md_static_num;
-  CXeleCffexMarketDataStaticField* md_static_field_list;
+  pthread_mutex_t mutex;
+  dict* tick_dick;
 };
 
-static int trader_mduser_api_static_inf_init(void* arg);
 
-static int trader_mduser_api_static_inf_load(void* self, const char* ip, int port);
+static unsigned int tickHash(const void *key) {
+    return dictGenHashFunction((const unsigned char *)key,
+                               strlen((const char*)key));
+}
 
-static int trader_mduser_api_static_inf_add(void* self, CXeleCffexMarketDataStaticField* pMarketDataStaticField);
+static void *tickKeyDup(void *privdata, const void *src) {
+    ((void) privdata);
+    int l1 = strlen((const char*)src)+1;
+    char* dup = (char*)malloc(l1 * sizeof(char));
+    strncpy(dup, (const char*)src, l1);
+    return dup;
+}
 
-static int trader_mduser_api_static_inf_sort(void* self);
+static int tickKeyCompare(void *privdata, const void *key1, const void *key2) {
+    int l1, l2;
+    ((void) privdata);
 
-static int trader_mduser_api_static_inf_search(void* self, CXeleCffexMarketDataStaticField* pMarketDataStaticField);
+    l1 = strlen((const char*)key1);
+    l2 = strlen((const char*)key2);
+    if (l1 != l2) return 0;
+    return memcmp(key1,key2,l1) == 0;
+}
 
-static int trader_mduser_api_static_inf_compare(CXeleCffexMarketDataStaticField* p, CXeleCffexMarketDataStaticField* q);
+static void tickKeyDestructor(void *privdata, void *key) {
+    ((void) privdata);
+    free((char*)key);
+}
 
-static int trader_mduser_api_static_inf_test();
+static dictType* tickDictTypeGet() {
+  static dictType tickDict = {
+    tickHash,
+    tickKeyDup,
+    NULL,
+    tickKeyCompare,
+    tickKeyDestructor,
+    NULL
+  };
+  return &tickDict;
+}
+
+static void trader_mduser_api_gd_tick_dict_init(trader_mduser_api_gd* self)
+{
+  dictType* tickDictType = tickDictTypeGet();
+  self->tick_dick = dictCreate(tickDictType,NULL);
+  pthread_mutex_init(&self->mutex, NULL);
+  return;
+}
+
+static int trader_mduser_api_gd_tick_dict_add(trader_mduser_api_gd* self, const char* instrument)
+{
+  int ret;
+  pthread_mutex_lock(&self->mutex);
+  ret = dictAdd(self->tick_dick, (void*)instrument, (void*)NULL);
+  pthread_mutex_unlock(&self->mutex);
+  return ret;
+}
+
+static int trader_mduser_api_gd_tick_dict_find(trader_mduser_api_gd* self, const char* instrument)
+{
+  int ret;
+  pthread_mutex_lock(&self->mutex);
+  ret = (NULL != dictFind(self->tick_dick, (void*)instrument));
+  pthread_mutex_unlock(&self->mutex);
+  return ret;
+}
+
+static void trader_mduser_api_gd_tick_dict_destory(trader_mduser_api_gd* self)
+{
+  dictRelease(self->tick_dick);
+  return;
+}
+
+
 
 trader_mduser_api_method* trader_mduser_api_gd_method_get()
 {
@@ -97,8 +177,6 @@ trader_mduser_api_method* trader_mduser_api_gd_method_get()
 
 void trader_mduser_api_gd_start(trader_mduser_api* self)
 {
-  //trader_mduser_api_static_inf_test();
-
   trader_mduser_api_gd* pImp = (trader_mduser_api_gd*)malloc(sizeof(trader_mduser_api_gd));
   self->pUserApi = (void*)pImp;
   int port;
@@ -109,11 +187,13 @@ void trader_mduser_api_gd_start(trader_mduser_api* self)
 
   pImp->remote_port = (unsigned short)port;
 
-  trader_mduser_api_static_inf_init((void*)pImp);
+  trader_mduser_api_gd_tick_dict_init(pImp);
+  
+  trader_mduser_api_on_rsp_user_login(self, 0, "OK");
+
+  sleep(1);
 
 	ret = pthread_create(&pImp->thread_id, NULL, trader_mduser_api_gd_thread, (void*)self);
-  
-	ret = pthread_create(&pImp->thread_tcp_id, NULL, trader_mduser_api_gd_tcp_thread, (void*)self);
 
   return ;
 }
@@ -127,11 +207,9 @@ void trader_mduser_api_gd_stop(trader_mduser_api* self)
   if(pImp->thread_id){
     pthread_join(pImp->thread_id, &ret);
   }
-  
-  if(pImp->thread_tcp_id){
-    pthread_join(pImp->thread_tcp_id, &ret);
-  }
 
+  trader_mduser_api_gd_tick_dict_destory(pImp);
+  
   free(pImp);
   self->pUserApi = (void*)NULL;
   
@@ -150,56 +228,40 @@ void trader_mduser_api_gd_logout(trader_mduser_api* self)
 
 void trader_mduser_api_gd_subscribe(trader_mduser_api* self, char* instrument)
 {
+  trader_mduser_api_gd* pImp = (trader_mduser_api_gd*)self->pUserApi;
+  trader_mduser_api_gd_tick_dict_add(pImp, instrument);
+  
   return ;
 }
 
-void gd_mduser_on_rtn_depth_market_data(void* arg, CXeleCffexLevelOneMarketData *pMarketData)
+void gd_x10_mduser_on_rtn_depth_market_data(void* arg, struct ShfeDataField *pMarketData)
 {
   trader_mduser_api* self = (trader_mduser_api*)arg;
   trader_tick oTick;
-  memset(&oTick, 0, sizeof(trader_tick));
+  trader_mduser_api_gd* pImp = (trader_mduser_api_gd*)self->pUserApi;
+  int found = trader_mduser_api_gd_tick_dict_find(pImp, pMarketData->instrument_id);
 
-  strcpy(oTick.InstrumentID, pMarketData->InstrumentID);
-  strcpy(oTick.UpdateTime, pMarketData->UpdateTime);
-  oTick.UpdateMillisec = pMarketData->UpdateMillisec;
-  oTick.BidPrice1 = pMarketData->BidPrice;
-  oTick.BidVolume1= pMarketData->BidVolume;
-  oTick.AskPrice1 = pMarketData->AskPrice;
-  oTick.AskVolume1= pMarketData->AskVolume;
-  oTick.LastPrice = pMarketData->LastPrice;
-
-  int ret;
-  CXeleCffexMarketDataStaticField marketDataStaticField;
-  strncpy(marketDataStaticField.InstrumentID, pMarketData->InstrumentID, sizeof(marketDataStaticField.InstrumentID));
-
-  ret = trader_mduser_api_static_inf_search((void*)self->pUserApi, &marketDataStaticField);
-  if(!ret){
-    oTick.UpperLimitPrice= marketDataStaticField.UpperLimitPrice;
-    oTick.LowerLimitPrice= marketDataStaticField.LowerLimitPrice;
-  }else{
-    oTick.UpperLimitPrice= oTick.AskPrice1 * 1.1;
-    oTick.LowerLimitPrice= oTick.BidPrice1 * 0.9;
+  if(!found){
+    return;
   }
   
-  //strcpy(oTick.TradingDay, marketDataStaticField.TradingDay);//
+  memset(&oTick, 0, sizeof(trader_tick));
+
+  strcpy(oTick.InstrumentID, pMarketData->m_symbol);
+  strcpy(oTick.TradingDay, "20210101");
+  strcpy(oTick.UpdateTime, pMarketData->m_update_time);
+  oTick.UpdateMillisec = pMarketData->m_millisecond;
+  oTick.BidPrice1 = pMarketData->m_bid_px;
+  oTick.BidVolume1 = pMarketData->m_bid_share;
+  oTick.AskPrice1 = pMarketData->m_ask_px;
+  oTick.AskVolume1 = pMarketData->m_ask_share;
+  oTick.UpperLimitPrice = pMarketData->m_ask_px * 1.1;
+  oTick.LowerLimitPrice = pMarketData->m_bid_px * 0.9;
+  oTick.LastPrice = pMarketData->m_last_px;
 
   trader_mduser_api_on_rtn_depth_market_data(self, &oTick);
 
   return;
-}
-void* trader_mduser_api_gd_tcp_thread(void* arg)
-{
-  trader_mduser_api* self = (trader_mduser_api*)arg;
-  trader_mduser_api_gd* pImp = (trader_mduser_api_gd*)self->pUserApi;
-  const char* tcp_ip = "172.37.88.12";
-  int tcp_port = 10007;
-  int ret = trader_mduser_api_static_inf_load((void*)pImp, tcp_ip, tcp_port);
-  if(ret < 0){
-    GDXELE_LOG("trader_mduser_api_static_inf_load ret=[%d]\n", ret);
-    exit(-1);
-  }
-
-  return (void*)NULL;
 }
 
 void* trader_mduser_api_gd_thread(void* arg)
@@ -208,7 +270,7 @@ void* trader_mduser_api_gd_thread(void* arg)
   trader_mduser_api_gd* pImp = (trader_mduser_api_gd*)self->pUserApi;
   int m_sock;
 
-	char line[RCV_BUF_SIZE] = "";
+	char line[MSG_BUF_SIZE] = "";
 
 	int n_rcved = -1;
 
@@ -220,7 +282,7 @@ void* trader_mduser_api_gd_thread(void* arg)
 	struct timeval tval;
 
   do{
-		m_sock = socket(AF_INET, SOCK_DGRAM, 0);
+		m_sock = socket(PF_INET, SOCK_DGRAM, 0);
 		if(-1 == m_sock) 
 		{
 			break;
@@ -245,19 +307,16 @@ void* trader_mduser_api_gd_thread(void* arg)
 			break;
 		}
 
-    GDXELE_LOG("trader_mduser_api_gd_thread bind\n");
 		struct sockaddr_in local_addr;
 		memset(&local_addr, 0, sizeof(local_addr));
 		local_addr.sin_family = AF_INET;
-		//local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    inet_aton(pImp->remote_ip, &local_addr.sin_addr);
+		local_addr.sin_addr.s_addr = htonl(INADDR_ANY);	
 		local_addr.sin_port = htons(pImp->remote_port);	//multicast port
 		if (bind(m_sock, (struct sockaddr*)&local_addr, sizeof(local_addr)) < 0)
 		{
 			break;
 		}
 
-    GDXELE_LOG("trader_mduser_api_gd_thread IP_ADD_MEMBERSHIP\n");
 		struct ip_mreq mreq;
 		mreq.imr_multiaddr.s_addr = inet_addr(pImp->remote_ip);	//multicast group ip
 		mreq.imr_interface.s_addr = inet_addr(pImp->local_ip);
@@ -273,8 +332,8 @@ void* trader_mduser_api_gd_thread(void* arg)
 			break;
 		}
 
-    int ret = 0;
-
+    pImp->loop_flag = 1;
+    flag = 0;
     while (pImp->loop_flag)
     {
     	FD_ZERO( &readSet);
@@ -294,7 +353,7 @@ void* trader_mduser_api_gd_thread(void* arg)
       }
       
     	socklen_t len = sizeof(sockaddr_in);
-    	n_rcved = recvfrom(m_sock, line, RCV_BUF_SIZE, 0, (struct sockaddr*)&muticast_addr, &len);
+    	n_rcved = recvfrom(m_sock, line, MSG_BUF_SIZE, 0, (struct sockaddr*)&muticast_addr, &len);
     	if ( n_rcved < 0) 
     	{
     		continue;
@@ -305,11 +364,16 @@ void* trader_mduser_api_gd_thread(void* arg)
     	}					
     	else
     	{
-    	  if(!ret){
-          GDXELE_LOG("gd_mduser_on_rtn_depth_market_data\n");
-          ret = 1;
+        //sw_mduser_on_rtn_depth_market_data(self, (CUstpFtdcDepthMarketDataField*)line);
+        if(!flag){
+          flag = 1;
+          GFXELE_LOG("sw_ctp_mduser_on_rtn_depth_market_data\n");
         }
-    		gd_mduser_on_rtn_depth_market_data(self, (CXeleCffexLevelOneMarketData*)(line + sizeof(CXeleCffexMdMessageHead)));				
+        //sw_ctp_mduser_on_rtn_depth_market_data(self, (CThostFtdcDepthMarketDataField*)line);
+        if(n_rcved > sizeof(struct ShfeDataField)){
+          GFXELE_LOG("n_rcved[%d] > DATA_WITH_HEAD\n", n_rcved);
+        }
+        gd_x10_mduser_on_rtn_depth_market_data(self, (struct ShfeDataField*)line);
     	}
     }
   }while(0);
@@ -361,215 +425,5 @@ int trader_mduser_api_gd_prase_url(const char* url, char* local_host, char* remo
   return 0;
 }
 
-int trader_mduser_api_static_inf_init(void* arg)
-{
-  GDXELE_LOG("trader_mduser_api_static_inf_init enter\n");
-
-  trader_mduser_api_gd* self = (trader_mduser_api_gd*)arg;
-  TAILQ_INIT(&self->md_static_list);
-  self->md_static_num = 0;
-  pthread_mutex_init(&self->thread_mutex, NULL);
-  
-  self->loop_flag = 1;
-
-  return 0;
-}
-
-
-int trader_mduser_api_static_inf_load(void* arg, const char* ip, int port)
-{
-  GDXELE_LOG("trader_mduser_api_static_inf_load enter\n");
-
-  trader_mduser_api_gd* self = (trader_mduser_api_gd*)arg;
-
-  // 建链
-  int ret = 0;
-  int fd = 0;
-  fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (fd == -1) {
-    return -1;
-  }
-
-  int on = 1;
-  ret = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
-  if (ret == -1) {
-    return -2;
-  }
-
-  GDXELE_LOG("trader_mduser_api_static_inf_load connect\n");
-  struct sockaddr_in tcp_server_addr;
-  memset(&tcp_server_addr, 0, sizeof(tcp_server_addr));
-  tcp_server_addr.sin_family = AF_INET;
-  inet_aton(ip, &tcp_server_addr.sin_addr);
-  tcp_server_addr.sin_port = htons(port);
-  do {
-    ret = connect(fd, (struct sockaddr *) &tcp_server_addr, sizeof(tcp_server_addr));
-    if (ret == -1) {
-      GDXELE_LOG("trader_mduser_api_static_inf_load connect failed\n");
-      usleep(500 * 1000);
-    }
-  }while((ret < 0) && self->loop_flag);
-
-  pthread_mutex_lock(&self->thread_mutex);
-
-  char buf[MSG_MAX_LEN];
-  while (true) {
-    GDXELE_LOG("trader_mduser_api_static_inf_load recv\n");
-    ret = recv(fd, buf, sizeof(CXeleCffexMdMessageHead) + sizeof(CXeleCffexMarketDataStaticField), MSG_WAITALL);
-    if (ret == 0) {
-      break;
-    } else if (ret < 0) {
-      break;
-    }
-    trader_mduser_api_static_inf_add(arg, (CXeleCffexMarketDataStaticField *) (buf + sizeof(CXeleCffexMdMessageHead)));
-  }
-  close(fd);
-  trader_mduser_api_static_inf_sort(arg);
-  pthread_mutex_unlock(&self->thread_mutex);
-
-  return 0;
-}
-
-int trader_mduser_api_static_inf_add(void* arg, CXeleCffexMarketDataStaticField* pMarketDataStaticField)
-{
-  trader_mduser_api_gd* self = (trader_mduser_api_gd*)arg;
-
-  xele_md_static_inf* p = (xele_md_static_inf*)malloc(sizeof(xele_md_static_inf));
-
-  memcpy(&p->data, pMarketDataStaticField, sizeof(CXeleCffexMarketDataStaticField));
-
-  GDXELE_LOG("p->data.InstrumentID=[%s]\n", p->data.InstrumentID);
-
-  TAILQ_INSERT_TAIL(&self->md_static_list, p, next);
-
-  self->md_static_num++;
-
-  return 0;
-}
-
-int trader_mduser_api_static_inf_sort(void* arg)
-{
-  trader_mduser_api_gd* self = (trader_mduser_api_gd*)arg;
-  self->md_static_field_list = (CXeleCffexMarketDataStaticField*)malloc(sizeof(CXeleCffexMarketDataStaticField) * self->md_static_num);
-  xele_md_static_inf* p;
-  CXeleCffexMarketDataStaticField* q;
-  CXeleCffexMarketDataStaticField tmp;
-
-  int i = 0;
-  int j = 0;
-  int ret;
-  TAILQ_FOREACH(p, &self->md_static_list, next){
-    if(0 == i){
-      memcpy(&self->md_static_field_list[i], &p->data, sizeof(CXeleCffexMarketDataStaticField));
-    }else{
-      j = i - 1;
-      while(j > -1){
-        ret = trader_mduser_api_static_inf_compare(&p->data, &self->md_static_field_list[j]);
-        if(ret < 0){
-          memcpy(&self->md_static_field_list[j+1], &self->md_static_field_list[j], sizeof(CXeleCffexMarketDataStaticField));
-          j--;
-        }else{
-          break;
-        }
-      }
-      memcpy(&self->md_static_field_list[j+1], &p->data, sizeof(CXeleCffexMarketDataStaticField));      
-    }
-    i++;
-  }
-  return 0;
-}
-
-int trader_mduser_api_static_inf_search(void* arg, CXeleCffexMarketDataStaticField* pMarketDataStaticField)
-{
-  trader_mduser_api_gd* self = (trader_mduser_api_gd*)arg;
-  
-  pthread_mutex_lock(&self->thread_mutex);
-
-	int low = 0;
-	int high = self->md_static_num - 1;
-	int mid;
-  int ret;
-  
-	while(low <= high){
-		mid = (low+high) / 2;
-    ret = trader_mduser_api_static_inf_compare(pMarketDataStaticField, &self->md_static_field_list[mid]);
-		if(0 == ret){
-      pMarketDataStaticField->UpperLimitPrice = self->md_static_field_list[mid].UpperLimitPrice;
-      pMarketDataStaticField->LowerLimitPrice = self->md_static_field_list[mid].LowerLimitPrice;
-      pthread_mutex_unlock(&self->thread_mutex);
-			return 0;    
-		}else if(ret > 0){
-			low = mid + 1;
-		}else{
-			high = mid - 1;
-    }
-	}
-  
-  pthread_mutex_unlock(&self->thread_mutex);
-	return -1; 
-
-}
-
-int trader_mduser_api_static_inf_compare(CXeleCffexMarketDataStaticField* p, CXeleCffexMarketDataStaticField* q)
-{
-  return strncmp(p->InstrumentID, q->InstrumentID, sizeof(p->InstrumentID));
-}
-
-int trader_mduser_api_static_inf_test()
-{
-  trader_mduser_api_gd* self = (trader_mduser_api_gd*)malloc(sizeof(trader_mduser_api_gd));
-  
-  TAILQ_INIT(&self->md_static_list);
-  self->md_static_num = 0;
-
-  CXeleCffexMarketDataStaticField field;
-  memset(&field, 0, sizeof(field));
-
-  strncpy(field.InstrumentID, "IC2010", sizeof(field.InstrumentID));
-  trader_mduser_api_static_inf_add((void*)self, &field);
-  
-  strncpy(field.InstrumentID, "IF2011", sizeof(field.InstrumentID));
-  trader_mduser_api_static_inf_add((void*)self, &field);
-  
-  strncpy(field.InstrumentID, "IH2103", sizeof(field.InstrumentID));
-  trader_mduser_api_static_inf_add((void*)self, &field);
-  
-  strncpy(field.InstrumentID, "IF2009", sizeof(field.InstrumentID));
-  trader_mduser_api_static_inf_add((void*)self, &field);
-  
-  strncpy(field.InstrumentID, "IC2011", sizeof(field.InstrumentID));
-  trader_mduser_api_static_inf_add((void*)self, &field);
-  
-  strncpy(field.InstrumentID, "IH2011", sizeof(field.InstrumentID));
-  trader_mduser_api_static_inf_add((void*)self, &field);
-  
-  strncpy(field.InstrumentID, "IH2009", sizeof(field.InstrumentID));
-  trader_mduser_api_static_inf_add((void*)self, &field);
-  
-  strncpy(field.InstrumentID, "IH2012", sizeof(field.InstrumentID));
-  trader_mduser_api_static_inf_add((void*)self, &field);
-  
-  strncpy(field.InstrumentID, "IF2103", sizeof(field.InstrumentID));
-  trader_mduser_api_static_inf_add((void*)self, &field);
-  
-  strncpy(field.InstrumentID, "IC2009", sizeof(field.InstrumentID));
-  trader_mduser_api_static_inf_add((void*)self, &field);
-  
-  strncpy(field.InstrumentID, "IF2012", sizeof(field.InstrumentID));
-  trader_mduser_api_static_inf_add((void*)self, &field);
-
-  trader_mduser_api_static_inf_sort((void*)self);
-
-  int i = 0;
-  for(i = 0; i < self->md_static_num; i++){
-    GDXELE_LOG("[%d][%s]\n", i, self->md_static_field_list[i].InstrumentID);
-  }
-
-  int ret = 0;
-  ret = trader_mduser_api_static_inf_search((void*)self, &field);
-  GDXELE_LOG("trader_mduser_api_static_inf_search ret=[%d]\n", ret);
-
-  exit(-1);
-}
 
 
